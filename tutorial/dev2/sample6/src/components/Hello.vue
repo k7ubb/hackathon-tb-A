9334abe3db29b567007de179e42c24fc1aeb04e8
qKota58wbp <y7g288app@yahoo.co.jp>
2023-08-22 17:10:46 +0900

    [6.6] データ受け渡し処理

--- tutorial/dev2/sample6/src/components/Hello.vue
+++ tutorial/dev2/sample6/src/components/Hello.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { inject } from "vue"
+import { useRouter } from "vue-router"
+
+const router = useRouter();
 
 const message = inject("message")
+
+const onClick = () =>{
+  message.value = "Hello Vue.js!"
+  router.push({ name: "hello"})
+}
+
 </script>
 
 <template>
