56d19a4e49cd632ce1f9378043f9c5395fab476f
komekui2023 <kubota.sou@vega.ocn.ne.jp>
2023-08-22 16:31:17 +0900

    [6.5] 変数埋め込み

--- tutorial/dev1/sample5/src/App.vue
+++ tutorial/dev1/sample5/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, reactive } from "vue"
 
-const title = "Sample5"
+const title = "Rakus"
 const message = ref("")
 const list = reactive([])
 
@@ -9,6 +9,7 @@ const addItem = () => {
   list.push(message.value)
   message.value = ""
 }
+
 </script>
 
 <template>
