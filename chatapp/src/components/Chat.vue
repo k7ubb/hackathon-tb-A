fd529fd26bc3c3ef2899d44249cde18bf4531393
komekui2023 <64680945+k7ubb@users.noreply.github.com>
2023-08-29 18:26:48 +0900

    shift+enterで改行

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -140,7 +140,7 @@ const registerSocketEvent = () => {
     <div class="'input-section mt-10'">
       <p class="login-user">ログインユーザ：{{ userName }}さん</p>
       <!-- Enter キーが押されたときに投稿可能 -->
-      <textarea @keydown.enter="onPublish" variant="outlined" placeholder="投稿文を入力してください " v-model="chatContent" rows="4" class="area"></textarea>
+      <textarea @keydown.enter.exact="onPublish" variant="outlined" placeholder="投稿文を入力してください " v-model="chatContent" rows="4" class="area"></textarea>
       <div class="mt-5">
           <button class="button-normal" @click="onPublish">投稿</button>
           <button class="button-normal util-ml-8px" @click="onMemo">メモ</button>
