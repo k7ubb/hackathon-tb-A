9156f4c5fc51ab192047709dffdef9dcaae58983
hoku-ya <hokutoprog@gmail.com>
2023-08-31 17:19:12 +0900

    ラジオボタン変更、文字数制限追加、chatContentとchat_type変数のprovide化、報連相ボタン用関数をまとめた

--- chatapp/src/App.vue
+++ chatapp/src/App.vue
@@ -3,10 +3,14 @@ import { provide, ref } from "vue"
 
 // #region reactive state
 const userName = ref("")
+const chatContent = ref("")
+const chat_type = ref("")
 // #endregion
 
 // #region global variable
 provide("userName", userName)
+provide("chatContent", chatContent)
+provide("chat_type", chat_type)
 // #endregion
 </script>
 
