0bff1d37abf3202d6b30d18ca24c910183399f85
komekui2023 <64680945+k7ubb@users.noreply.github.com>
2023-09-04 02:00:25 +0900

    リファクタリング

--- chatapp/src/App.vue
+++ chatapp/src/App.vue
@@ -4,15 +4,11 @@ import { provide, ref } from "vue"
 // #region reactive state
 const userName = ref("")
 const chatContent = ref("")
-const chat_type = ref("")
-const chat_id = ref(0)
 // #endregion
 
 // #region global variable
 provide("userName", userName)
 provide("chatContent", chatContent)
-provide("chat_type", chat_type)
-provide("chat_id", chat_id)
 // #endregion
 </script>
 
