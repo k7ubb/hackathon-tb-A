3cd0a2b28899a5e26517a5144ca31e3137cd4c16
0-ryt <ryt09.r0n01@gmail.com>
2023-09-03 11:16:07 +0900

    オンラインユーザを表示する

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,24 +1,37 @@
 let lastUser=null
+let lastPublishTime = {};
+let connectedUsers = [];
 
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
+    const user = JSON.parse(data).username;
+    console.log(user)
+    if (!connectedUsers.includes(user)) {
+      connectedUsers.push(user);
+    }
+    console.log(connectedUsers)
+    io.sockets.emit("onlineUsers", JSON.stringify(connectedUsers));
+    // 新たに入室したユーザにも
+    // socket.emit("onlineUsers", JSON.stringify(connectedUsers));
+
     socket.broadcast.emit("enterEvent", data)
   })
 
-  // 退室メッセージをクライアントに送信する
   socket.on("exitEvent", (data) => {
-    socket.broadcast.emit("exitEvent", data)
-  })
+    socket.broadcast.emit("exitEvent", data);
+  });
 
-  // 投稿メッセージを送信する
   // 投稿メッセージを送信する
   socket.on("publishEvent", (data) => {
     const user = JSON.parse(data).username
-    if (user === lastUser) {
-      socket.emit("error", "連続して投稿することはできません。")
-      return
+
+    if (user === lastUser ) {
+      socket.emit("error", "連続して投稿することはできません。");
+      lastUser = user
+      return;
     }
+
     lastUser = user
     io.sockets.emit("publishEvent", data)
   })
