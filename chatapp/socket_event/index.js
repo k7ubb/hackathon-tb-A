20c7238a668c1d9dfa29600972ba7e590b51d0b5
0-ryt <ryt09.r0n01@gmail.com>
2023-09-03 16:48:53 +0900

    オンラインユーザの選択&メンション機能の実装

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,9 +1,9 @@
 let lastUser=null
-let lastPublishTime = {};
 let connectedUsers = [];
 
 export default (io, socket) => {
-  // 入室メッセージをクライアントに送信する
+  socket.emit("onlineUsers", JSON.stringify(connectedUsers));
+
   socket.on("enterEvent", (data) => {
     const user = JSON.parse(data).username;
     console.log(user)
@@ -12,13 +12,17 @@ export default (io, socket) => {
     }
     console.log(connectedUsers)
     io.sockets.emit("onlineUsers", JSON.stringify(connectedUsers));
-    // 新たに入室したユーザにも
-    // socket.emit("onlineUsers", JSON.stringify(connectedUsers));
 
     socket.broadcast.emit("enterEvent", data)
   })
 
   socket.on("exitEvent", (data) => {
+    const user = JSON.parse(data).username;
+    const index = connectedUsers.indexOf(user);
+    if (index > -1) {
+      connectedUsers.splice(index, 1);
+    }
+    io.sockets.emit("onlineUsers", JSON.stringify(connectedUsers));
     socket.broadcast.emit("exitEvent", data);
   });
 
