3ff04fb7aaba62a064020eca500ee4b09f30fdd3
0-ryt <ryt09.r0n01@gmail.com>
2023-09-08 20:26:57 +0900

    デザイン(適当)

--- chatapp/src/components/Reply.vue
+++ chatapp/src/components/Reply.vue
@@ -65,25 +65,33 @@ const registerSocketEvent = () => {
 <template>
   <div class="reply">
     <h1>コミュニケーションラボ</h1>
-    <p class="reply-top">返信専用画面</p>
-    <div class="input-section mt-10">
-      <!-- <p class="reply-message">{{ message }}</p> -->
-      <p class="reply-message" v-html="message"></p>
-      <textarea v-model="replyContent" rows="4" class="area" placeholder="上記のメッセージに対しての返信文を入力"></textarea>
-      <div class="mt-5">
-        <button class="button-normal" @click="onPublishReply">返信</button>
+    <div class="info">
+      <p class="login-user">ログインユーザ：{{ userName }}さん</p>
+    </div>
+
+    <div class="post">
+      <p class="reply-top">返信専用画面</p>
+      <div class="input-section mt-10">
+        <p class="reply-message" v-html="message"></p>
+        <textarea v-model="replyContent" rows="4" class="area" placeholder="上記のメッセージに対しての返信文を入力"></textarea>
+        <div class="mt-5">
+          <button class="button-normal" @click="onPublishReply">返信</button>
+        </div>
       </div>
     </div>
-    <div class="reply-section mt-10">
-      <h3>返信</h3>
-      <ul>
-        <li v-for="(reply, i) in store.state.replyList.slice().reverse()" :key="i">
-          <div></div>
-          <pre>{{reply.username + "さん " +"["+new Date(reply.unixtime).toLocaleString("jp-JP")+"]" }}</pre>
-          <pre>{{ reply.message }}</pre>
-        </li>
-      </ul>
-    </div>
+
+ <div class="reply-section mt-10">
+    <h3>返信</h3>
+    <ul>
+      <li v-for="(reply, i) in store.state.replyList.slice().reverse()" :key="i" class="reply-card">
+        <div class="reply-content">
+          <pre class="reply-header">{{reply.username + "さん " +"["+new Date(reply.unixtime).toLocaleString("jp-JP")+"]" }}</pre>
+          <pre class="reply-message">{{ reply.message }}</pre>
+        </div>
+      </li>
+    </ul>
+  </div>
+
     <router-link to="/chat" class="link">
       <button type="button" class="button-normal button-exit">戻る</button>
     </router-link>
